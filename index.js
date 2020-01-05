fc40291363feabc25e8852e0685dfbaca2adf2a5
trageddy <59534885+trageddy@users.noreply.github.com>
2020-01-05 18:32:34 +0100

    Update index.js
    Updated sharding manager

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-// test
-d
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./bot.js', { token: 'XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxX' });
+
+manager.spawn();
+manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
